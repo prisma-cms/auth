83068f42c8d5d3d4dfa86799a21a51c61ab08b25
Fi1osof <n.lanets@modxclub.ru>
2018-10-03 17:31:32 +0300

    1.0.4-beta

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-export default 'Welcome to @prisma-cms/prisma-auth'
+
+
+const jwt = require('jsonwebtoken')
+
+export const getUserId = async function (ctx, token) {
+  
+  const Authorization = token || (ctx.request && ctx.request.get('Authorization'))
+  
+  if (Authorization) {
+    const token = Authorization.replace('Bearer ', '')
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+
+    const userExists = await ctx.db.exists.User({
+      id: userId,
+    });
+
+    if(!userExists){
+      throw new AuthError()
+    }
+
+    return userId
+  }
+
+  throw new AuthError()
+}
+
+
+class AuthError extends Error {
+  constructor() {
+    super('Not authorized')
+  }
+}
+
+export default getUserId;
+
+module.exports = {
+  getUserId,
+}
